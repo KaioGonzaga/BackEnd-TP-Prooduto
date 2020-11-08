0399d61d442325d97dead762a166a7d9d631deb1
kaio <kaiogonzaga2017@outlook.com>
2020-11-08 20:21:13 -0300

    atualização para subir no heroku

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,26 @@
 // Define a string de conexão com o banco de dados
+require('dotenv/config');
+ 
+// Define a string de conexão com o banco de dados
 module.exports = {
     development: {
         database: {
-            host: 'localhost',
-            port: 3306,
-            name: 'DB_USUARIO',
-            dialect: 'mysql',
-            user: 'root',
-            password: 'Brasilpenta'
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            name: process.env.DB_NAME,
+            dialect: process.env.DB_DIALECT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD
         }
     },
-    production:{
+    production: {
         database: {
             host: process.env.DB_HOST,
-            host: process.env.DB_PORT
+            port: process.env.DB_PORT,
+            name: process.env.DB_NAME,
+            dialect: process.env.DB_DIALECT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD       }
         }
     }
-}
+    
